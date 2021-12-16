759473608cc9131f972a078392df1b798fb1336e
Alessandro <alessandro01desideri@gmail.com>
2021-12-16 17:46:46 +0100

    Commento richeista utente

--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -1,5 +1,16 @@
-prompt('Inserisci un numero da 1 a 5')
-function numCasuale(minNum , maxNum){
+// Chiedo utente num e pari/dispari
+let userNumber = parseInt(prompt('Inserisci un numero da 1 a 5'));
+let scelta = prompt('Scegli tra pari e dispari')
+
+console.log(userNumber)
+
+
+function computerNumber(minNum , maxNum){
     let num = Math.floor(Math.random() * ((maxNum + 1) - minNum)) + minNum;
     return num;
 }
+
+let machineNumber = computerNumber(1, 5);
+console.log(machineNumber)
+numero = userNumber + machineNumber;
+console.log(numero);
