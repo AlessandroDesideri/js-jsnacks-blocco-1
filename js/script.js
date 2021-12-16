7e520bd3e5b069d4e5169f9cf3b6c743a008751c
Alessandro <alessandro01desideri@gmail.com>
2021-12-16 17:06:41 +0100

    Inserimento const funzione

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const word = prompt('Inserire parola')
+
+function palindroma(string) {
+    const arrayParola = string.split('');
+    const arrayReverse = arrayParola.reverse('');
+    const parolaInvertita = arrayReverse.join('')
+}
